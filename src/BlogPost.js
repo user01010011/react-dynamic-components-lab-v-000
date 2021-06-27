87328b0ee8f9f609a6e3e3c76fc4980c407222fd
Shasha Yan <shashayan@gmail.com>
2021-06-27 15:46:53 -0500

    commit tree 11ed4a164312f82a303890a46bb33e44c50fcde7

--- src/BlogPost.js
+++ src/BlogPost.js
@@ -10,7 +10,7 @@ export default class BlogPost extends Component {
       "I am no longer accepting the things I cannot change. I am changing the things I cannot accept. - Angela Davis",
       "If you don't understand, ask questions. If you're uncomfortable about asking questions, say you are uncomfortable about asking questions and then ask anyway. It's easy to tell when a question is coming from a good place. Then listen some more. Sometimes people just want to feel heard. Here's to possibilities of friendship and connection and understanding. - Chimamanda Ngozi Adichie"
     ]
-    
+
     return (
       <div id="blog-post" className="wrapper">
         Just like moons and like suns,<br/>
@@ -22,6 +22,7 @@ export default class BlogPost extends Component {
         <Comment commentText={comments[0]} />
         <Comment commentText={comments[1]} />
         <Comment commentText={comments[2]} />
+        
       </div>
     );
   }
